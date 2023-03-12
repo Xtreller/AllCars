49fcc88f34bcbd92758f3c11e71794adf330558b
Xtrell <vladsito@gmail.com>
2023-03-12 23:21:18 +0200

    Initial changes

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  SafeAreaView,
+} from "react-native";
 export default function App() {
+  const AutoMotoLogo = require("./src/image/ezgif-3-011b04688a.jpg");
+  const AutoBgLogo = require("./src/image/ezgif-3-011b04688a.jpg");
+  const carsLogo = require("./src/image/ezgif-3-2091f6109b.png");
+  const MobileLogo = require("./src/image/mobile.png");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <NavigationContainer>
+    <SafeAreaView style={styles.container}>
+   
+      <Image
+        source={require("./src/image/logo_transparent.png")}
+        style={styles.baner}
+      ></Image>
+      <TouchableOpacity
+        style={[styles.touchables, { backgroundColor: "#50C346" }]}
+      >
+        <Image source={AutoMotoLogo} style={{ width: 200, height: 60 }}></Image>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.touchables, { backgroundColor: "#FF6600" }]}
+      >
+        <Image
+          source={carsLogo}
+          style={{ width: 200, height: 60 }}
+          resizeMode={"contain"}
+        ></Image>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.autobg, { backgroundColor: "#38618C" }]}>
+        <Text>Аuto.bg</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.touchables, { backgroundColor: "#0099FF" }]}
+      >
+        <Image
+          source={MobileLogo}
+          style={{ width: 200, height: 60 }}
+          resizeMode={"contain"}
+        ></Image>
+      </TouchableOpacity>
+    </SafeAreaView>
+    // {/* </NavigationContainer> */}
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#40a7f1",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  baner: {
+    width: "75%",
+    height: "20%",
+    position: "absolute",
+    top: 80,
+    minWidth: "55%",
+    minHeight: 60,
+  },
+  touchables: {
+    flexDirection: "row",
+    padding: 5,
+    backgroundColor: "",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "ligthblue",
+    overflow: "hidden",
+    marginBottom: 15,
+    minWidth: "55%",
+    minHeight: 60,
+  },
+  autobg: {
+    flexDirection: "row",
+    padding: 5,
+    backgroundColor: "",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "blacak  ",
+    overflow: "hidden",
+    marginBottom: 15,
+    minWidth: "55%",
+    minHeight: 60,
   },
 });
